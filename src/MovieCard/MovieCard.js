bef906b54d30339efcbd3098bfad4486b2d983e5
nahytar <nahytar@gmail.com>
2019-01-31 01:28:51 -0300

    Se agregan filtros por categoria y ranking.

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -8,6 +8,7 @@ class MovieCard extends React.Component {
       <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${this.props.image}`} />
       <Card.Body>
         <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>Ranking: {this.props.ranking}</Card.Text>
       </Card.Body>
     </Card>
   );
--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -6,11 +6,12 @@ class MovieList extends React.Component {
   render = () => (
     <div>
       <h3>{this.props.title}</h3>
-      <Row>
+      <Row className="movie-list">
         {this.props.movies.map(movie => (
           <MovieCard
             title={movie.title}
             image={movie.poster_path}
+            ranking={movie.vote_average}
             key={movie.id}
           />
         ))}
